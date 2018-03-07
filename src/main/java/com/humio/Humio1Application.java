ceb5e85ef5a73a74c8c0f8ad1892fc1e4c34d73d
Rod Johnson <johnsonroda@gmail.com>
2018-03-07 15:25:49 +0000

    Initial commit from Atomist

---
+++ src/main/java/com/humio/Humio1Application.java
@@ -0,0 +1,12 @@
+package com.humio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Humio1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Humio1Application.class, args);
+	}
+}
